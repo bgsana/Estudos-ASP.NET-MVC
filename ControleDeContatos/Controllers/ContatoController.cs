8f394f11a91e064e739e21cdf9c16e628705298c
bgsana <bgs.ana08@gmail.com>
2025-12-18 22:12:31 -0300

    Iniciado o sistema de cadastro de clientes

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Controllers;
+
+public class ContatoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Criar()
+    {
+        return View();
+    }
+    public IActionResult Editar()
+    {
+        return View();
+    }
+    public IActionResult Excluir()
+    {
+        return View();
+    }
+}
